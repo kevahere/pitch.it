55fe2798e6fcef3bac7833b5b31d10b7d2ee798f
kevahere <kevahere@gmail.com>
2018-09-10 06:50:50 +0300

    configured user model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
-from . import DB
+from . import db
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True, index = True)
+    password = db.Column(db.String(255))
+    #Define relationship between user and pitch
+    pitch = db.relationship('Pitch',backref = 'user',lazy = 'dynamic')
+
