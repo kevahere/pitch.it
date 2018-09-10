65b4c6e996135f25eacf943de2d7bddb764923c2
kevahere <kevahere@gmail.com>
2018-09-10 07:16:54 +0300

    added pitch table

--- app/models.py
+++ app/models.py
@@ -9,3 +9,17 @@ class User(UserMixin,db.Model):
     #Define relationship between user and pitch
     pitch = db.relationship('Pitch',backref = 'user',lazy = 'dynamic')
 
+    def __repr__(self):
+        return f'User{self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id =  db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255))
+    pitch_body = db.Column(db.String)
+    #Define the relationship between a User and a pitch
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f'Pitch {self.title}'
