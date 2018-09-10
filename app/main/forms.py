62e7bb29bc360d01cb0799e97e443ef048d07ef8
kevahere <kevahere@gmail.com>
2018-09-10 12:55:32 +0300

    added form logic for pitch form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,1 +1,9 @@
-# Optional
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField,SubmitField
+from models import Pitch
+from wtforms.validators import DataRequired
+
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Category Title', validators=[DataRequired()])
+    body = TextAreaField('Enter your Pitch here', validators=[DataRequired()])
+    submit = SubmitField('Submit')
