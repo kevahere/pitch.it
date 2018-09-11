692f3b82c3127275c99f2dc4a7fa9fb8d5d19003
kevahere <kevahere@gmail.com>
2018-09-11 10:39:03 +0300

    added register,login and logout routes on views

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,3 +4,38 @@ from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
 from flask_login import login_user, current_user, logout_user
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.commit()
+        flash('You are now signed up!')
+        return redirect(url_for('auth.login'))
+        title = 'Create an account'
+    return render_template('auth/register.html', form = form,title = title)
+
+@auth.route('/login',methods = ["GET","POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.index'))
+        else:
+            flash('Login unsuccessful.Invalid username or pawword.')
+    title = "Login"
+    return render_template('auth/login.html',form = form ,title = title)
+
+@auth.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
